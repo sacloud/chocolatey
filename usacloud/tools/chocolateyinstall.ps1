f0c9b2256982d3770c765445f9750793ed2ef7e7
223n <tatami@cmail.plala.or.jp>
2017-06-21 23:26:48 +0900

    update usacloud ver.0.0.12 delete admin tag. #4

--- usacloud/tools/chocolateyinstall.ps1
+++ usacloud/tools/chocolateyinstall.ps1
@@ -6,8 +6,8 @@ $ErrorActionPreference = 'Stop';
 
 $packageName= $env:ChocolateyPackageName
 $toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$url        = 'https://github.com/sacloud/usacloud/releases/download/v0.0.11/usacloud_windows-386.zip'
-$url64      = 'https://github.com/sacloud/usacloud/releases/download/v0.0.11/usacloud_windows-amd64.zip'
+$url        = 'https://github.com/sacloud/usacloud/releases/download/v0.0.12/usacloud_windows-386.zip'
+$url64      = 'https://github.com/sacloud/usacloud/releases/download/v0.0.12/usacloud_windows-amd64.zip'
 
 $packageArgs = @{
   packageName   = $packageName
@@ -15,9 +15,9 @@ $packageArgs = @{
   url           = $url
   url64bit      = $url64
   softwareName  = 'usacloud*'
-  checksum      = '9B9D051D6AD79028E365A25546F8595A5DE7C731509498359186294ABFEF6AF4F2403403BF7C3BE784486E8DD275D5CFAB11DF3CD902F85E515495FF8504C5CD'
+  checksum      = '510588EA7466C6301F6CC38B1FACE7BEF8160D28E6E589B5A066FB2EA8F95DE49BABB2C6FE485D42F0471096DFADDB25D675E93E00E8475E680FE2834CB32F6A'
   checksumType  = 'sha512' #default is md5, can also be sha1, sha256 or sha512
-  checksum64    = '848A27F21D95607D098407E87836F2D419ED40F2067BF59C6307482D188C4760934105DB60AD022FFA0F416AF97A10316FD455E427493DE1DE5F1694298D72AE'
+  checksum64    = '309A1304E5A0654190FC13C4461CEA81143C99FCBAFB1062C5F6C574F795E9AC3C24A3F66F7EC419FF59C6927BD933B6859A5D4C9EDABA897B3EC63A2667FBB5'
   checksumType64= 'sha512'
 }
 
