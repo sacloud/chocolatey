43ce0dbcd72c28ac2a3ff5875bacaace5be2fab7
223n <tatami@cmail.plala.or.jp>
2017-07-04 22:38:51 +0900

    update terraform-provider-sakuracloud ver.0.10.3.

--- terraform-provider-sakuracloud/tools/chocolateyuninstall.ps1
+++ terraform-provider-sakuracloud/tools/chocolateyuninstall.ps1
@@ -1,8 +1,4 @@
-﻿# IMPORTANT: Before releasing this package, copy/paste the next 2 lines into PowerShell to remove all comments from this file:
-#   $f='c:\path\to\thisFile.ps1'
-#   gc $f | ? {$_ -notmatch "^\s*#"} | % {$_ -replace '(^.*?)\s*?[^``]#.*','$1'} | Out-File $f+".~" -en utf8; mv -fo $f+".~" $f
-
-$ErrorActionPreference = 'Stop';
+﻿$ErrorActionPreference = 'Stop';
 
 $packageArgs = @{
   packageName   = $env:ChocolateyPackageName
@@ -11,8 +7,5 @@ $packageArgs = @{
   zipFileName64 = 'terraform-provider-sakuracloud_windows-amd64.zip'
 }
 
-$uninstalled = $false
-
-# Only necessary if you did not unpack to package directory - see https://chocolatey.org/docs/helpers-uninstall-chocolatey-zip-package
 Uninstall-ChocolateyZipPackage -PackageName $packageArgs['packageName'] -ZipFileName $packageArgs['zipFileName']
 Uninstall-ChocolateyZipPackage -PackageName $packageArgs['packageName'] -ZipFileName $packageArgs['zipFileName64']
