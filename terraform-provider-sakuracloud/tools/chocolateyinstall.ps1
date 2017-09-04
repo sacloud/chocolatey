6e22f82575b1b501014f4ee52d80fd3108eb3c79
223n <tatami@cmail.plala.or.jp>
2017-09-04 23:05:18 +0900

    update terraform-provider-sakuracloud Ver.0.12.0

--- terraform-provider-sakuracloud/tools/chocolateyinstall.ps1
+++ terraform-provider-sakuracloud/tools/chocolateyinstall.ps1
@@ -3,11 +3,11 @@
 $packageName  = $env:ChocolateyPackageName;
 $toolsDir     = Join-Path $env:APPDATA "terraform.d/plugins/"
 $softwareName = 'terraform-provider-sakuracloud*';
-$url          = 'https://github.com/sacloud/terraform-provider-sakuracloud/releases/download/v0.11.0/terraform-provider-sakuracloud_windows-386.zip';
-$url64        = 'https://github.com/sacloud/terraform-provider-sakuracloud/releases/download/v0.11.0/terraform-provider-sakuracloud_windows-amd64.zip';
+$url          = 'https://github.com/sacloud/terraform-provider-sakuracloud/releases/download/v0.12.0/terraform-provider-sakuracloud_windows-386.zip';
+$url64        = 'https://github.com/sacloud/terraform-provider-sakuracloud/releases/download/v0.12.0/terraform-provider-sakuracloud_windows-amd64.zip';
 $checkSum     = 'sha512';
-$hash32       = '0903E3AEA47B8B276709084D989B4E39604DEB5019CDD721D39D38F05ED07DFDAE64103084EF3BFBD5502EB777FD8F6799DB3BE008D943BFF3ECF9EBF9350030';
-$hash64       = 'E939CBD8D99078161F65EB02D9EAF7D6B4197A3A9BB7DC7660A68FA29A82C537B535B18386B94F7B3117BE9797D4ED036B79D872514B01D31F633F50E9B47D6D';
+$hash32       = '11180BE310D27B3075B2FE0F2AE1A655F1F68E3A2FE8CFA016B0D7F60C966FFA34AF89B42A1914F45DFEC05FA25CA91FE7FD990A3F9B04404CF30A914EF82E5E';
+$hash64       = 'FED47A0212A285CBC253576E00FF2B760AB1F4D76ADE4E22EC56DD301AE76C07E2172220EA77C4AE77CEBCDC1BDD99B63330D691758F480FF9787DB4F24BAB02';
 
 $packageArgs = @{
   packageName   = $packageName
