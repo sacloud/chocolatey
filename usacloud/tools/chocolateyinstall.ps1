721fd2ae300113778e7a3326156d4e125b115324
223n <tatami@cmail.plala.or.jp>
2017-11-12 02:53:28 +0900

    update v0.4.0

--- usacloud/tools/chocolateyinstall.ps1
+++ usacloud/tools/chocolateyinstall.ps1
@@ -3,11 +3,11 @@
 $packageName  = $env:ChocolateyPackageName;
 $toolsDir     = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)";
 $softwareName = 'usacloud*';
-$url32        = 'https://github.com/sacloud/usacloud/releases/download/v0.3.1/usacloud_windows-386.zip';
-$url64        = 'https://github.com/sacloud/usacloud/releases/download/v0.3.1/usacloud_windows-amd64.zip';
+$url32        = 'https://github.com/sacloud/usacloud/releases/download/v0.4.0/usacloud_windows-386.zip';
+$url64        = 'https://github.com/sacloud/usacloud/releases/download/v0.4.0/usacloud_windows-amd64.zip';
 $hashType     = 'sha512';
-$hash32       = 'F0B330BF3BC7B98A4588ABDAA5EA6062186B397E72EBE73D730BB3AA9000304B32F83F87C35EE1AC309FA739C05B40DA1B6DAADF392F876BE32F528EDE2560B3';
-$hash64       = '80C2EC19D0A808AEBE4AA1856AE4A8A0A11D4429DAC369B2C7C39B5C628A86AFAD64583596119E479CC6C086F3EDC4592AD7419C78EA2EC389EE67E6DD90CB0F';
+$hash32       = 'A43292E7E4AD99223FC6E311F495C1D52F98BEB5319555E737A7B61AB5EB0FC23038ABAB039E8C1ACB890A0FDAC028E0ABBAE54CA2E08625D020E87BB3216453';
+$hash64       = '62258B1CD69DCF21750E202FCEBB90200DD7578C967426125F6069B68B62B824CBFE0E3C8C4C838B0A80ECF3D55CC46312B4B8851AEED9243FBCE92F5E62E007';
 
 $packageArgs = @{
   packageName   = $packageName
